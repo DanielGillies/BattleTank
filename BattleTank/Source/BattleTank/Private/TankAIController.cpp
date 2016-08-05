cf09a3fbed3927cc918a15db6aba9efc59d99230
Daniel Gillies <danny@skully.com>
2016-08-05 16:08:31 -0700

    Created tank AI and connected to dropped tanks in scene. AI Tanks can find player tank. Working on playertank xhair aiming.

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("AIController not possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController possessing: %s"), *ControlledTank->GetName());
+	}
+
+	auto PlayerTank = GetPlayerTank();
+
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s unable to find player tank"), *ControlledTank->GetName())
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s found player tank named %s"), *ControlledTank->GetName(), *PlayerTank->GetName());
+	}
+
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
+}
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -15,8 +15,13 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
+	virtual void BeginPlay() override;
+	
 	ATank* GetControlledTank() const;
-	
-	
-	
+
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
+
+	// Aim barrel at where xhair is pointing
+	void AimTowardsCrosshair();
 };
